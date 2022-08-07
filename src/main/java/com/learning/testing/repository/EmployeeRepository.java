645891876e0e4c64fadb3c7f715940d36192e48f
FAWRY\Youssif.Saad <youssif.saad@fawry.com>
2022-08-07 11:35:10 +0200

    Commit: Handle delete employee, jpql query unit test. Code refactor

--- src/main/java/com/learning/testing/repository/EmployeeRepository.java
+++ src/main/java/com/learning/testing/repository/EmployeeRepository.java
@@ -2,8 +2,14 @@ package com.learning.testing.repository;
 
 import com.learning.testing.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
     Employee getByEmail(String email);
+
+
+    @Query("select emp from Employee emp where emp.firstName=:firstName")   // it may return list but just for testing
+    Employee getByFirstName(@Param("firstName") String firstName);
 }
--- src/test/java/com/learning/testing/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/learning/testing/repository/EmployeeRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.learning.testing.repository;
 
 import com.learning.testing.entity.Employee;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,18 +22,23 @@ public class EmployeeRepositoryTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    private Employee employee;
+
+    @BeforeEach
+    public void setup(){
+        employee = Employee.builder()
+                .firstName("Youssif")
+                .lastName("Tawfik")
+                .email("youssif.tawfik96@gmail.com")
+                .salary(4000.0)
+                .build();
+    }
 
     @Test
     @DisplayName("JUnit test for Save Employee")
     public void givenEmployee_whenSave_thenReturnSavedEmployee() {
 
         // given - setup
-        Employee employee = Employee.builder()
-                .firstName("Youssif")
-                .lastName("Tawfik")
-                .email("youssif.tawfik96@gmail.com")
-                .salary(4000.0)
-                .build();
 
         //when - action
         Employee savedEmployee = employeeRepository.save(employee);
@@ -46,20 +53,13 @@ public class EmployeeRepositoryTest {
     public void givenEmployees_whenFindAll_thenReturnAllEmployees() {
 
         // given - setup
-        Employee employee1 = Employee.builder()
-                .firstName("Youssif")
-                .lastName("Tawfik")
-                .email("youssif.tawfik96@gmail.com")
-                .salary(4000.0)
-                .build();
-
         Employee employee2 = Employee.builder()
                 .firstName("Martin")
                 .lastName("Cuber")
                 .email("martin@gmail.com")
                 .salary(3000.0)
                 .build();
-        List<Employee> employees = List.of(employee1, employee2);
+        List<Employee> employees = List.of(employee, employee2);
         employeeRepository.saveAll(employees);
 
         //when - action
@@ -75,12 +75,12 @@ public class EmployeeRepositoryTest {
     public void givenEmail_whenFindByEmail_thenReturnEmployeeObject() {
 
         // given - setup
-        Employee employee = Employee.builder()
-                .firstName("Youssif")
-                .lastName("Tawfik")
-                .email("youssif.tawfik96@gmail.com")
-                .salary(4000.0)
-                .build();
+//        Employee employee = Employee.builder()
+//                .firstName("Youssif")
+//                .lastName("Tawfik")
+//                .email("youssif.tawfik96@gmail.com")
+//                .salary(4000.0)
+//                .build();
         employeeRepository.save(employee);
 
         //when - action
@@ -96,12 +96,6 @@ public class EmployeeRepositoryTest {
     public void givenEmployee_whenUpdate_thenReturnUpdatedEmployee() {
 
         // given - setup
-        Employee employee = Employee.builder()
-                .firstName("Youssif")
-                .lastName("Tawfik")
-                .email("youssif.tawfik96@gmail.com")
-                .salary(4000.0)
-                .build();
         employeeRepository.save(employee);
 
         //when - action
@@ -113,7 +107,33 @@ public class EmployeeRepositoryTest {
         //then - verify
         assertThat(updatedEmployee.getFirstName()).isEqualTo("Smith");
         assertThat(updatedEmployee.getEmail()).isEqualTo(employeeDb.getEmail());
+    }
 
+    @Test
+    @DisplayName("JUnit test for delete employee")
+    public void givenEmployee_whenDelete_thenAssertNotFound(){
+
+        // given - setup
+        employeeRepository.save(employee);
+        // when - action
+        employeeRepository.delete(employee);
+        Optional<Employee> deletedEmployee = employeeRepository.findById(employee.getId());
+        // then - verify
+        assertThat(deletedEmployee).isEmpty();
+    }
+
+
+    @Test
+    @DisplayName("JUnit test for Test JPQL Operation")
+    public void givenEmployee_whenGetByFirstName_thenReturnEmployeeObject(){
+
+        // given - setup
+        employeeRepository.save(employee);
+        // when - action
+        String firstName="Youssif";
+        Employee employeeDb=employeeRepository.getByFirstName(firstName);
+        // then - verify
+        assertThat(employeeDb).isNotNull();
     }
 
 
