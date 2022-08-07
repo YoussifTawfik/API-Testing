befb0986ed5f3ad9681087d9c85773b99ff59f2e
FAWRY\Youssif.Saad <youssif.saad@fawry.com>
2022-08-07 09:03:51 +0200

    Commit: Handle update employee unit test

--- src/test/java/com/learning/testing/repository/EmployeeRepositoryTest.java
+++ src/test/java/com/learning/testing/repository/EmployeeRepositoryTest.java
@@ -5,17 +5,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 
 /*
-* @DataJpaTest:
-* scan all classes that annotated with entity or repository
-* roll back all transaction when finish testing the method
-* */
+ * @DataJpaTest:
+ * scan all classes that annotated with entity or repository
+ * roll back all transaction when finish testing the method
+ * */
 @DataJpaTest
 public class EmployeeRepositoryTest {
 
@@ -24,11 +22,11 @@ public class EmployeeRepositoryTest {
 
 
     @Test
-    @DisplayName("Save Employee Test Method")
-    public void givenEmployee_whenSave_thenReturnSavedEmployee(){
+    @DisplayName("JUnit test for Save Employee")
+    public void givenEmployee_whenSave_thenReturnSavedEmployee() {
 
         // given - setup
-        Employee employee=Employee.builder()
+        Employee employee = Employee.builder()
                 .firstName("Youssif")
                 .lastName("Tawfik")
                 .email("youssif.tawfik96@gmail.com")
@@ -44,24 +42,24 @@ public class EmployeeRepositoryTest {
     }
 
     @Test
-    @DisplayName("Find All Test Method")
-    public void givenEmployees_whenFindAll_thenReturnAllEmployees(){
+    @DisplayName("JUnit test for Find All")
+    public void givenEmployees_whenFindAll_thenReturnAllEmployees() {
 
         // given - setup
-        Employee employee1=Employee.builder()
+        Employee employee1 = Employee.builder()
                 .firstName("Youssif")
                 .lastName("Tawfik")
                 .email("youssif.tawfik96@gmail.com")
                 .salary(4000.0)
                 .build();
 
-        Employee employee2=Employee.builder()
+        Employee employee2 = Employee.builder()
                 .firstName("Martin")
                 .lastName("Cuber")
                 .email("martin@gmail.com")
                 .salary(3000.0)
                 .build();
-        List<Employee> employees=List.of(employee1,employee2);
+        List<Employee> employees = List.of(employee1, employee2);
         employeeRepository.saveAll(employees);
 
         //when - action
@@ -74,10 +72,10 @@ public class EmployeeRepositoryTest {
 
     @Test
     @DisplayName("JUnit test for Find By Email")
-    public void givenEmail_whenFindByEmail_thenReturnEmployeeObject(){
+    public void givenEmail_whenFindByEmail_thenReturnEmployeeObject() {
 
         // given - setup
-        Employee employee=Employee.builder()
+        Employee employee = Employee.builder()
                 .firstName("Youssif")
                 .lastName("Tawfik")
                 .email("youssif.tawfik96@gmail.com")
@@ -86,10 +84,36 @@ public class EmployeeRepositoryTest {
         employeeRepository.save(employee);
 
         //when - action
-        Employee retrievedEmployee=employeeRepository.getByEmail(employee.getEmail());
+        Employee retrievedEmployee = employeeRepository.getByEmail(employee.getEmail());
 
         //then - verify
         assertThat(retrievedEmployee).isNotNull();
+    }
+
+
+    @Test
+    @DisplayName("JUnit test for Update Employee")
+    public void givenEmployee_whenUpdate_thenReturnUpdatedEmployee() {
+
+        // given - setup
+        Employee employee = Employee.builder()
+                .firstName("Youssif")
+                .lastName("Tawfik")
+                .email("youssif.tawfik96@gmail.com")
+                .salary(4000.0)
+                .build();
+        employeeRepository.save(employee);
+
+        //when - action
+        Employee employeeDb = employeeRepository.findById(employee.getId()).get();
+        employeeDb.setFirstName("Smith");
+        employeeDb.setEmail("smith@gmail.com");
+        Employee updatedEmployee=employeeRepository.save(employeeDb);
+
+        //then - verify
+        assertThat(updatedEmployee.getFirstName()).isEqualTo("Smith");
+        assertThat(updatedEmployee.getEmail()).isEqualTo(employeeDb.getEmail());
+
     }
 
 
